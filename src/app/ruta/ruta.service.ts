3eb316877beba7a61678e6d60c41c61c0cda4145
RONALD MAMANI NINAJA <RONALD MAMANI NINAJA>
2017-03-15 18:11:17 -0500

    terminado ruta.service

--- src/app/ruta/ruta.service.ts
+++ src/app/ruta/ruta.service.ts
@@ -5,5 +5,75 @@ import {Observable} from 'rxjs';
 @Injectable()
 
 export class RutaService{
-    
+    private baseUrl: string = 'http://controlbus-controlbus.44fs.preview.openshiftapps.com/bus/rest/ruta/';
+	private baseUrl2: string = 'http://controlbus-controlbus.44fs.preview.openshiftapps.com/bus/rest/rutadetalle/';
+    //private baseUrl: string = 'http://localhost:8081/bus/rest/ruta/';
+    constructor(private http: Http) { }
+	getAllRutaByEm(emId: number) {
+		return this.http
+				.get(this.baseUrl+ "getallrutabyem?emId="+emId) 
+				.map((r: Response) => r.json() )             
+				.catch(this.handleError);
+	}
+	getRutaById(ruId:number){
+		return this.http
+			.get(this.baseUrl+ ruId )
+			.map((r: Response) => r.json() )
+			.catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+	}	
+    newRuta(){
+		return this.http
+			.get(this.baseUrl+ 'new')
+			.map((r: Response) => r.json() )
+			.catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+
+	}
+	saveRuta(ruta:Object){
+		//si en caso se quiere enviar mas de un objeto
+		//let data=JSON.stringify({ Album: tramiteMov, User: tramiteMov, UserToken: tramiteMov })
+		return this.http.post(this.baseUrl+ "save", ruta) // ...using post request
+                         .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+	}
+	deleteRuta(id:number){
+		return this.http.delete(this.baseUrl+ "delete/"+id) // ...using post request
+                         .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+	}
+	//Objeto RutaDetalle 
+	getAllRutaDetalleByRu(ruId:number){
+		return this.http
+			.get(this.baseUrl2+'ruid/'+ruId )
+			.map((r: Response) => r.json() )
+			.catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+	}
+	newRutaDetalle(){
+		return this.http
+			.get(this.baseUrl2+ 'new')
+			.map((r: Response) => r.json() )
+			.catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+
+	}
+	deleteRutaDetalleByRu(ruId:number){
+		return this.http.delete(this.baseUrl+ "delete/ruid/"+ruId) // ...using post request
+                         .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+	}
+	saveRutaDetalle(rutaDetalle:Object[]){
+		//si en caso se quiere enviar mas de un objeto
+		//let data=JSON.stringify({ Album: tramiteMov, User: tramiteMov, UserToken: tramiteMov })
+		return this.http.post(this.baseUrl2+ "save", rutaDetalle) // ...using post request
+                         .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+	}
+    // this could also be a private method of the component class
+	handleError (error: any) {
+		// log error
+		// could be something more sofisticated
+		let errorMsg = error.message;// || `Yikes! There was was a problem with our hyperdrive device and we couldn't retrieve your data!`
+		console.error(errorMsg);
+
+		// throw an application level error
+		return Observable.throw(errorMsg);
+	}
 }
