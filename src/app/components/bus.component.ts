72c1285408a7f6c6b5487f13e9020708ba7dec31
victordamian159 <victor.luque.pro@gmail.com>
2017-06-06 22:26:34 -0500

    06-06-2017__10:27pm

--- src/app/components/bus.component.ts
+++ src/app/components/bus.component.ts
@@ -7,6 +7,7 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class BusComponent implements OnInit{
+    /* OBJETOS*/
     bus={
         BuId : null,
         SuEmId : null,
@@ -28,7 +29,65 @@ export class BusComponent implements OnInit{
         UsId:null,
         UsFechaReg:"",
     }
+
+    /*ARRAYS */
+    _bus=[]; /*ARRAY BD*/
+    _gbus=[]; /*ARRAY GRILLA*/
+
+    /* VARIEBLES*/
+
+    /* DISPLAY MODAL*/
+
+    /* OTRAS VARIABLES*/
+    
     ngOnInit(){
-        
+        this._bus=[
+            {BuEmp : "CAMOTE", BuPlaca : "CSD 1231", BuCombustible : "COCA COLA", BuSOAT : "3242341213NLKML", BuModelo : "MARIPOSA"},
+            {BuEmp : "PAPA HUAYRO", BuPlaca : "XZ 131", BuCombustible : "VENCINA", BuSOAT : "90809090FFFML", BuModelo : "ABEJITA"},
+            {BuEmp : "OLLUCO", BuPlaca : "ÑD 1441", BuCombustible : "KEROSENE", BuSOAT : "02I3029MD029", BuModelo : "TORTUGA"},
+            {BuEmp : "RUTA NRO 18", BuPlaca : "PP 199", BuCombustible : "GASOLINA", BuSOAT : "D2LK3D2D23988293", BuModelo : "CORRE CAMINOS"},
+            {BuEmp : "ALBERT EINSTEIN", BuPlaca : "DD 331", BuCombustible : "GLP", BuSOAT : "384F3J9843JN38 ", BuModelo : "CARACOL"},
+            {BuEmp : "XILOFONO", BuPlaca : "AAW 151", BuCombustible : "PETROLEO", BuSOAT : "2320000000SCDCD", BuModelo : "ESCARABAJO"},
+            {BuEmp : "RUTA 13", BuPlaca : "MWM 100", BuCombustible : "CHICHA MORADA", BuSOAT : "ML342FM2MLKMLKM", BuModelo : "BAGONETA"},
+            {BuEmp : "RUTA UTA", BuPlaca : "AAA 1351", BuCombustible : "PISCO", BuSOAT : "22225555500S1233CD", BuModelo : "MINI BAND"},
+            {BuEmp : "RUTA 13", BuPlaca : "MWM 100", BuCombustible : "ALCOHOL", BuSOAT : "ML342FM2MLKMLKM", BuModelo : "GAVIOTA"}
+        ];
+        this.mgBus();
+    }
+    
+    /* NUEVO OBJETO BUS*/ 
+    nuevoBus(){
+        console.log("nuevo");
+    }
+
+    /* NUEVO OBJETO BUS*/ 
+    editarBus(bus : Object){
+        console.log("editar");
+    }
+
+    /* NUEVO OBJETO BUS*/ 
+    eliminarBus(idbus : number){
+        console.log("eliminar");
+    }
+
+    /* MOSTRAR DATOS EN GRILLA*/
+    mgBus(){
+        for(let bus of this._bus){
+            this._gbus.push({
+                nro : 0,
+                BuEmp : bus.BuEmp,
+                BuPlaca : bus.BuPlaca,
+                BuCombustible : bus.BuCombustible,
+                BuSOAT : bus.BuSOAT,
+                BuModelo : bus.BuModelo
+            });
+        }
+        for(let i=0; i<this._bus.length;i++){
+            this._gbus[i].nro=i+1;
+        }
+    }
+
+    onRowSelectBus(event){
+        console.log("seleccionado :s");
     }
 }
